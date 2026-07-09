f9a406b5415579633fc9ee732c22f9d08940a506
mahesh577-dell <69102807+mahesh577-dell@users.noreply.github.com>
2026-07-09 20:40:59 +0530

    Update variables.tf

--- environments/tms-dev/variables.tf
+++ environments/tms-dev/variables.tf
@@ -1,5 +1,28 @@
-variable "project_id"  { description = "GCP Project ID"; type = string }
-variable "region"      { description = "GCP Region"; type = string; default = "asia-south1" }
-variable "vpc_name"    { description = "VPC name"; type = string; default = "tms-dev-vpc" }
-variable "vpc_cidr"    { description = "VPC CIDR"; type = string; default = "10.60.0.0/16" }
-variable "environment" { description = "Environment name"; type = string; default = "dev" }
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "asia-south1"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+  default     = "tms-dev-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.60.0.0/16"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
