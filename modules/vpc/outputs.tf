fa8768e62601aaffdda1e9c0a4b6187501205b62
golla mahesh <mahesh.golla@ankercloud.com>
2026-07-09 20:55:08 +0530

    fix: Fix all output files syntax

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,11 @@
-output "vpc_id"        { value = google_compute_network.vpc.id }
-output "vpc_name"      { value = google_compute_network.vpc.name }
-output "vpc_self_link" { value = google_compute_network.vpc.self_link }
+output "vpc_id" {
+  value = google_compute_network.vpc.id
+}
+
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "vpc_self_link" {
+  value = google_compute_network.vpc.self_link
+}
